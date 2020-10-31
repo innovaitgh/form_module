a80d2b3edabb76345d7f35322a8f141a4b0c2b02
Kwame Shyd <kwameshyd@gmail.com>
2020-10-31 19:08:55 +0000

    defined useForm

--- dist/with_resource.js
+++ dist/with_resource.js
@@ -13,6 +13,8 @@ var _router = require("router");
 
 var _ui = require("ui");
 
+var _use_form = _interopRequireDefault(require("./use_form"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
@@ -63,7 +65,7 @@ var _default = function _default(Component, _ref) {
       }
     }, []);
 
-    var form = useForm(formProps);
+    var form = (0, _use_form["default"])(formProps);
 
     _react["default"].useEffect(function () {
       if (!resouce) return;
